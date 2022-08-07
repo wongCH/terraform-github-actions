fb2ce7ab7505ac9bd4ddec7c594af63899ca3dc0
wong_hun@gmail.com <wong_.hun@gmail.com>
2022-08-07 15:31:45 +0800

    point to backend terraform cloud

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+    required_providers  {
+        aws = {
+            source = "hashicorp/aws"
+            version = "3.26.0"
+        }
+        random  = {
+            source = "hashicorp/random"
+            version = "3.0.1"
+        }
+
+        required_version = "~> 1.0"
+
+        backend "remote" {
+            oraganization = "acloudguru-terraform-wch"
+
+            workspaces {
+                name = "demo-github-actions"
+            }
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "random_pet" "sg" {}
+
+resource "aws_instance" "web" {
+    ami = "ami-090fa75af13c156b4"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+    user_data =  <<-EOF
+                #!/bin/bash
+                echo 'hello workd' > index.html
+                nohup buxybox httpd -f -p 8080 &
+                EOF
+}
+
+resource "aws_security_group" "web-sg" {
+    name = "${random_pet.sg.id}-sg"
+
+    ingress  {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  
+}
+
+output "web-address" {
+    value = "${aws_instance.web.public_dns}:8080"
+}
